c92b3b1ddd2b9ad4379864551fbd9fbca9e2c52d
its-alikhokher <iamaliraza777@gmail.com>
2026-02-23 12:38:02 +0500

    Update copyright information from Muqeet Mughal to ALi Raza across multiple files

--- zkt_integration/zkt_biometrix_integration/doctype/attendance_device_log/attendance_device_log.js
+++ zkt_integration/zkt_biometrix_integration/doctype/attendance_device_log/attendance_device_log.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2025, Muqeet Mughal and contributors
+// Copyright (c) 2025, ALi Raza and contributors
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Attendance Device Log", {
